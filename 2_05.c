3281d9eb5fa87d5b5f517adf686d113c57c6de23
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2022-07-09 03:27:36 +0700

    Create 2_05.c

---
+++ 2_05.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, tong;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    tong=a+b+c;
+    printf("%d\n", tong);
+    printf("%f", tong/3.0);
+}
